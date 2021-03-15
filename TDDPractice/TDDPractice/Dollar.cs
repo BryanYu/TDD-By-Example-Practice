68927e7ad47ac77c2f196282186256a4be1f9956
Bryan.Yu <bryan.yu@johnsontechinc.com>
2021-03-15 21:31:32 +0800

    Compile Success, Tests Case Error

---
+++ TDDPractice/TDDPractice/Dollar.cs
@@ -0,0 +1,16 @@
+namespace TDDPractice
+{
+    public class Dollar
+    {
+        public int Amount { get; set; }
+        
+        public Dollar(int amount)
+        {
+        }
+
+        public void Times(int multiplier)
+        {
+            
+        }
+    }
+}
